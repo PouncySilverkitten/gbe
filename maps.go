ead10a882b8c0d4ab4c4baf92e49ce6c47f14434
PouncySilverkitten <struan@duncan-wilson.co.uk>
2017-05-06 11:54:56 +0100

    Maps, range and functions

---
+++ maps.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map: ", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1: ", v1)
+
+    fmt.Println("len: ", len(m))
+
+
+
+    delete(m, "k2")
+    fmt.Println("Map: ", m)
+
+    _, prs := m["k2"]
+    fmt.Println("Prs: ", prs)
+
+    n:= map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("Map: ", n)
+
+}
